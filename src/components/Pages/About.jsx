eb323db62255240831f86dcba4213e917d971bd9
Miagotobene <miagotobene@gmail.com>
2024-07-18 00:50:01 -0400

    feat: added responsive features and routes to homepage

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+         <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
+        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
+        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
+        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
+        </div>
+        <br />
+         <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
+        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
+        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
+        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
+        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
+        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
+        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
+      </div> 
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
+        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
+        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
+        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
+      </div>
+    </div>
+   
+
+  )
+}
+
+export default About;
