f4a9fe23777df4e98ec824527ce0d099a8fd4589
tflinch <tflinch16@gmail.com>
2024-07-17 23:55:07 -0400

    feat: add Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home'
 
 
 const App = () => {
 
-  const [theme, setTheme]= useState('light')
+  const [theme, setTheme] = useState('light')
   return (
     <div className={`container ${theme} App`}>
-      <Navbar  theme={theme} setTheme={setTheme}/>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Home theme={theme} />
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = (props) => {
+    const theme = props.theme;
+
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+export default Home;
