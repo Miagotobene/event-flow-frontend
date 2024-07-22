4d6c95ec8972e908dd2b9ac69cef669f99aa7c24
Miagotobene <miagotobene@gmail.com>
2024-07-21 21:21:32 -0400

    feat: updated the about section or page

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,38 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './about.css'
 
 const About = () => {
   return (
-    <div>
-         <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
-        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
-        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
-        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
-        </div>
-        <br />
-         <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
-        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
-        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
-        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
-        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
-        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
-        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
-      </div> 
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Reiciendis consequuntur voluptatum omnis distinctio eligendi eius dolore reprehenderit sint? 
-        Molestiae aut iusto tenetur perspiciatis quasi vel rem? Reprehenderit, facilis! Unde dolores provident nisi est inventore dicta ad id, accusamus porro nostrum iusto enim ipsam, laborum ratione! Minima, accusantium ducimus. 
-        Ut totam vitae id hic provident cum ad dolorum dignissimos. In eligendi nesciunt consequuntur ducimus optio exercitationem quis laborum laboriosam doloremque odio. 
-        Consectetur incidunt quam ducimus eum labore in nemo praesentium reprehenderit? Reiciendis quasi sit asperiores cum blanditiis! Iste, quisquam. Iste eligendi ut atque ad eos doloribus fugiat eveniet ipsam deserunt distinctio!
-      </div>
-    </div>
+    <body>
+    <header>
+    </header>
+    <main>
+        <section className="about-section">
+             <h1>About Us</h1>
+            <h2>Our Story</h2>
+            <p>Event Flow started with a simple idea: to streamline event planning and management. Our founders, passionate about both technology and event organization, envisioned a platform that would empower event organizers with powerful tools.</p>
+        </section>
+        <section className="about-section">
+            <h2>Our Mission</h2>
+            <p>Event Flow: Your ultimate companion for flawless event management. Streamline your planning process, manage guests effortlessly, and ensure every detail falls into place seamlessly. 
+              Welcome to Event Flow, your go-to platform for effortless event planning and booking. 
+              Whether you're organizing a workshop, a wedding reception, or a community fundraiser, 
+              Event Flow empowers you to create memorable experiences with ease. Our intuitive app ensures every detail is handled smoothly. 
+              Simplify your event planning journey today with Event Flow and unlock a world of convenience at your fingertips.
+              Sign up to Event Flow now and turn your vision into reality.</p>
+        </section>
+    </main>
+</body>
+    
    
 
   )
